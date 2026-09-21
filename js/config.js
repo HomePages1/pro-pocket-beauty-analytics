e45a2f612f11835bbfb02805330d352f852122ed
HomePages1 <tamaaricontents@gmail.com>
2026-09-21 14:33:46 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.ANALYTICS_CONFIG = {
+  gasUrl: "https://script.google.com/macros/s/AKfycbxGCd0FMmQMjIGhkYm84MNeIyS92KvWjfPLRZlFIPq7XAojrB575LntoHawg3nuz9eDxg/exec",
+  apiKey: "Pro Pocket Beauty Analytics0904"
+};
