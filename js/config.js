ec2fe29796f3c82175c1e45303ab4566c5d5c417
HomePages1 <tamaaricontents@gmail.com>
2026-09-21 14:59:01 +0900

    Delete js/config.js

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.ANALYTICS_CONFIG = {
-  gasUrl: "https://script.google.com/macros/s/AKfycbxGCd0FMmQMjIGhkYm84MNeIyS92KvWjfPLRZlFIPq7XAojrB575LntoHawg3nuz9eDxg/exec",
-  apiKey: "Pro Pocket Beauty Analytics0904"
-};
